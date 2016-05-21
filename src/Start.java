aa255016c613430804535cd5cd6090b0c7d60cb7
Ruslan <Ruslan@Ruslans-MacBook-Pro.local>
2016-05-21 16:03:51 +0300

    commit

---
+++ src/Start.java
@@ -0,0 +1,13 @@
+
+public class Start {
+
+    public static void main(String[] args) {
+        int k=0;
+        while (k<10) {
+            k+=2;
+            if (k<0) {
+                continue;
+            }
+        }
+    }
+}
