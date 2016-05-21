3e663ed93dd8ea23053a5864725235eb0a16ad6e
Ruslan <Ruslan@Ruslans-MacBook-Pro.local>
2016-05-21 20:34:32 +0300

    ThirdCommit

--- src/test/Start.java
+++ src/test/Start.java
@@ -3,15 +3,18 @@ package test;
 public class Start {
 
     public static void main(String[] args) {
-        Car car = new Car();
+        Car c1 = new Car();
+        Car c2 = c1;
 
-        car.setName("Toyota");
-        car.setName("BMW");
-       // car = null;
+        c1.setName("Toyota");
+        System.out.println("c2.getName() = " + c2.getName());
+
+        c2.setName("BMW");
+        System.out.println("c1 = " + c1.getName());
 
 
 
-        System.out.println("I have " + car.getName());
+
 
     }
 }
