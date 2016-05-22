fd78ff175f354657210ca9c59a5ec3c04c0f0e9f
Ruslan <Ruslan@Ruslans-MacBook-Pro.local>
2016-05-22 12:13:32 +0300

    newCommit

--- src/test/Car.java
+++ src/test/Car.java
@@ -8,8 +8,34 @@ public class Car {
         return name;
     }
 
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Car)) return false;
+//
+//        Car car = (Car) o;
+//
+//        return name.equals(car.name);
+//
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return name.hashCode();
+//    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
     public void setName(String name) {
         this.name = name;
+
+
+
     }
 }
 
--- src/test/Start.java
+++ src/test/Start.java
@@ -4,14 +4,11 @@ public class Start {
 
     public static void main(String[] args) {
         Car c1 = new Car();
-        Car c2 = c1;
+        c1.setName("BMW");
+        Car c2 = new Car();
+        c2.setName("Toyota");
 
-        c1.setName("Toyota");
-        System.out.println("c2.getName() = " + c2.getName());
-
-        c2.setName("BMW");
-        System.out.println("c1 = " + c1.getName());
-
+        System.out.println("c2 = " + c2);
 
 
 
